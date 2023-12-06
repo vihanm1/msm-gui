b0c71d3475b7b465ef773f6f87610298de3f688e
vihanm1 <146213135+vihanm1@users.noreply.github.com>
2023-12-06 17:17:05 +0000

    ABC

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,60 +1,59 @@
 class MoviesController < ApplicationController
-  def update
-      m_id = params.fetch ("the_id")
-      matching_records = Movie.where({ :id => the_id })
-
-      the_movie.title = params.fetch("the_title")
-      the_movie.year = params.fetch("the_year")
-      the_movie.duration = params.fetch("the_duration")
-      the_movie.description = params.fetch("the_description")
-      the_movie.image = params.fetch("the_image")
-      the_movie.director_id = params.fetch("the_director_id")
-
-      the_movie.save
-
-      redirect_to("/movies/#{the_movie.id}")
-  
-  end
-  def create
-    m = Movie.new
-    m.title = params.fetch("the_title")
-    m.year = params.fetch("the_year")
-    m.duration = params.fetch("the_duration")
-    m.description = params.fetch("the_description")
-    m.image = params.fetch("the_image")
-    m.director_id = params.fetch("the_director_id")
-
-    m.save
-
-    redirect_to("/movies")
-  end
-
-  def destroy
-      the_id = params.fetch("an_id")
-
-      matching_records = Movie.where({ :id => the_id })
-
-      the_movie = matching_records.at(0)
-
-      the_movie.destroy
-
-      redirect_to("/movies")
-  end
-
-
   def index
-    matching_movies = Movie.all
-    @list_of_movies = matching_movies.order({ :created_at => :desc })
+    @list_of_movies = Movie.all.order({ :created_at => :desc })
 
     render({ :template => "movie_templates/index" })
   end
 
   def show
     the_id = params.fetch("path_id")
-
-    matching_movies = Movie.where({ :id => the_id })
-    @the_movie = matching_movies.at(0)
+    @the_movie = Movie.find(the_id)
 
     render({ :template => "movie_templates/show" })
+  end
+
+  def create
+    @the_movie = Movie.new
+    @the_movie.title = params.fetch("query_title")
+    @the_movie.year = params.fetch("query_year")
+    @the_movie.duration = params.fetch("query_duration")
+    @the_movie.description = params.fetch("query_description")
+    @the_movie.image = params.fetch("query_image")
+    @the_movie.director_id = params.fetch("query_director_id")
+
+    if @the_movie.valid?
+      @the_movie.save
+      redirect_to("/movies", { :notice => "Movie created successfully." })
+    else
+      redirect_to("/movies", { :notice => "Movie failed to create successfully." })
+    end
+  end
+
+  def update
+    the_id = params.fetch("path_id")
+    @the_movie = Movie.find(the_id)
+
+    @the_movie.title = params.fetch("query_title")
+    @the_movie.year = params.fetch("query_year")
+    @the_movie.duration = params.fetch("query_duration")
+    @the_movie.description = params.fetch("query_description")
+    @the_movie.image = params.fetch("query_image")
+    @the_movie.director_id = params.fetch("query_director_id")
+
+    if @the_movie.valid?
+      @the_movie.save
+      redirect_to("/movies/#{@the_movie.id}", { :notice => "Movie updated successfully."} )
+    else
+      redirect_to("/movies/#{@the_movie.id}", { :alert => "Movie failed to update successfully." })
+    end
+  end
+
+  def destroy
+    the_id = params.fetch("path_id")
+    @the_movie = Movie.find(the_id)
+
+    @the_movie.destroy
+
+    redirect_to("/movies", { :notice => "Movie deleted successfully."} )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,21 +5,20 @@ Rails.application.routes.draw do
   get("/directors/eldest", { :controller => "directors", :action => "min_dob" })
 
   get("/directors", { :controller => "directors", :action => "index" })
+  post("/insert_director", { :controller => "directors", :action => "create" })
   get("/directors/:path_id", { :controller => "directors", :action => "show" })
-  post("/insert_directors_record", { :controller => "directors", :action => "create" })
-  get("/delete_directors/:an_id", { :controller => "directors", :action => "destroy"})
-  post("/directors_directors_record/the_id", { :controller => "directors", :action => "update"})
+  post("/modify_director/:path_id", { :controller => "directors", :action => "update" })
+  get("/delete_director/:path_id", { :controller => "directors", :action => "destroy" })
 
   get("/movies", { :controller => "movies", :action => "index" })
+  post("/insert_movie", { :controller => "movies", :action => "create" })
   get("/movies/:path_id", { :controller => "movies", :action => "show" })
-  post("/insert_movie_record", { :controller => "movies", :action => "create" })
-  get("/delete_movie/:an_id", { :controller => "movies", :action => "destroy"})
-  post("/movie_movie_record/the_id", { :controller => "movies", :action => "update"})
+  post("/modify_movie/:path_id", { :controller => "movies", :action => "update" })
+  get("/delete_movie/:path_id", { :controller => "movies", :action => "destroy" })
   
   get("/actors", { :controller => "actors", :action => "index" })
+  post("/insert_actor", { :controller => "actors", :action => "create" })
   get("/actors/:path_id", { :controller => "actors", :action => "show" })
-  post("/insert_actors_record", { :controller => "actors", :action => "create" })
-  get("/delete_actors/:an_id", { :controller => "actors", :action => "destroy"})
-  post("/actors_actors_record/the_id", { :controller => "actors", :action => "update"})
-
+  post("/modify_actor/:path_id", { :controller => "actors", :action => "update" })
+  get("/delete_actor/:path_id", { :controller => "actors", :action => "destroy" })
 end
